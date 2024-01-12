7d4b45e9dfadca13e15706cbc00fcaa1a91d2c8a
Дар'я Таневська <2daryachernata2@gmail.com>
2024-01-12 17:26:33 +0100

    Initial commit

---
+++ src/hw1/Hw1.java
@@ -0,0 +1,15 @@
+package hw1;
+
+public class Hw1 {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 9;
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
